bf012c8a568a8fb02a99df91b09025fc61d4c6e1
Koakoalla <104795085+Koakoalla@users.noreply.github.com>
2023-04-25 15:36:16 +0300

    initial

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      Chatroulette by Koakoalla
+    </div>
+  );
+};
+
+export default Home;
---
+++ server/index.ts
@@ -0,0 +1,21 @@
+/* eslint-disable no-console */
+import { createServer } from "http";
+
+import express from "express";
+import next, { NextApiHandler } from "next";
+// import { Server } from 'socket.io';
+
+const port = parseInt(process.env.PORT || "3000", 10);
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const nextHandler: NextApiHandler = nextApp.getRequestHandler();
+
+nextApp.prepare().then(async () => {
+  const app = express();
+  const server = createServer(app);
+  app.all("*", (req: any, res: any) => nextHandler(req, res));
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
